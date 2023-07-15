29eda7ae2490d74f94f6a9fc6a6fe2130029dca6
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-07-15 23:03:14 +0100

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,12 @@
-<footer class="bg-dark text-white py-3">
-    <div class="container">
-        <!-- Place des éléments du footer ici -->
+<footer class="bg-dark text-white py-5">
+    <div class="container py-5">
+        <div class="row justify-content-center py-5">
+            <div class="col-md-4">
+                <p class="text-center">
+                    copyright &copy; <?= Date('Y');?>
+                </p>
+            </div>
+        </div>
     </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
